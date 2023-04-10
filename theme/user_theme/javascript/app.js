78cc811ae17bb36bac96b6b6365d2ace2c6fc5ac
Moobbot <ngotam24082001@gmail.com>
2023-04-10 23:34:14 +0700

    first update

---
+++ theme/user_theme/javascript/app.js
@@ -0,0 +1,23 @@
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+(function ($) {
+  "use strict";
+  $(document).ready(function () {
+    var bannerCarousel = document.querySelector("#bannerCarousel");
+    var carousel = new bootstrap.Carousel(bannerCarousel, {
+      interval: 2000,
+      pause: "hover",
+      wrap: false,
+    });
+  });
+  $(document).mouseup(function (e) {});
+  window.addEventListener("mouseover", function (e) {
+    var container = $("name");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      //code here
+    }
+  });
+  window.addEventListener("load", function () {});
+})(window.jQuery);
